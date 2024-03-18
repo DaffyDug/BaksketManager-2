ffb14968948838231e84854da6a3d50a2065a30d
Александр Александр <stripalol@inbox.ru>
2024-03-18 08:50:13 +0700

    Добавьте файлы проекта.

---
+++ BaksketManager 2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        ICommandBasket[] CommandBaskets = new ICommandBasket[]
+        {
+        new AddProduct(),
+        new RemoveProduct(),
+        new PrintInfo(),
+        new PrintInfoCategoryProduct(),
+        new GetProductRangePrice()
+        };
+        while (true)
+        {
+            ShowMenu(CommandBaskets);
+            if (InputHelper.Input("выберите что вы будете делать: ", 1, CommandBaskets.Length, out int inputvalue))
+            {
+                CommandBaskets[inputvalue - 1].Run();
+            }
+        }
+    }
+    static void ShowMenu(ICommandBasket[] CommandBaskets)
+    {
+        for (int i = 0; i < CommandBaskets.Length; i++)
+        {
+            ICommandBasket item = CommandBaskets[i];
+            Console.WriteLine($"{i + 1}--{item}");
+        }
+    }
+}
